a8245f94f78afbc3d91b12ca5326ed0950db9a8f
Sixcha <paulbora_akcan@hotmail.com>
2022-03-30 11:55:12 +0200

    Initial Commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const req = require('express/lib/request');
+
+const express = require('express')();
+const app = express();
+const PORT = 8080;
+
+app.listen(
+    PORT,()=> console.log(`http://localhost:${PORT}`)
+)
+
+app.use( express.json() )
+
+app.get('/text', (req, res) =>
+{
+    res.status(200).send({
+        Duck: 'Quack',
+        What: 'Quackkk'
+    })
+})
